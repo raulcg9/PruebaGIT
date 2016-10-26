d4244eb53cf912e4962c89f31d082f114c45318d
Raul <raulwork9@gmail.com>
2016-10-26 10:46:49 +0200

    Creada clase Suma

---
+++ Suma.java
@@ -0,0 +1,13 @@
+
+public class Suma {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 6;
+		int suma = a+b;
+		 System.out.println("la suma de "+a+" y "+b+" es "+suma);
+
+	}
+
+}
