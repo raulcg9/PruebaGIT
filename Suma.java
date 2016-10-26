1dc3b47a4921ba5cac84b90185d2aa44804eca9c
Raul <raulwork9@gmail.com>
2016-10-26 10:52:03 +0200

    añadida la resta

--- Suma.java
+++ Suma.java
@@ -6,7 +6,9 @@ public class Suma {
 		int a = 5;
 		int b = 6;
 		int suma = a+b;
+		int resta = a-b;
 		 System.out.println("la suma de "+a+" y "+b+" es "+suma);
+		 System.out.println(resta);
 
 	}
 
